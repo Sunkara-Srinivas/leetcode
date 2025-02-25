901c5630ecc54dac349ab058956e0a16cc905fac
Sunkara-Srinivas <srinivas.sunkara.2006@gmail.com>
2025-02-25 14:27:48 +0530

    Time: 1290 ms (58.15%), Space: 0B (100%) - LeetHub

---
+++ 1327-last-person-to-fit-in-the-bus/1327-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,3 @@
+/* Write your PL/SQL query statement below */
+with cte as (select person_name ,sum(weight) over(order by turn)as total from queue)
+select person_name from (select person_name from cte where total<=1000  order by total desc)where ROWNUM=1;
