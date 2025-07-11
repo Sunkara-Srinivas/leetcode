5560465c44760e53a0391f72f429db337cebba99
Sunkara-Srinivas <srinivas.sunkara.2006@gmail.com>
2025-07-11 22:16:18 +0530

    Time: 0 ms (100%), Space: 41.2 MB (15.24%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isUgly(int n) {
+        boolean res;
+        if(n==1)
+        {
+            return true;
+        }
+        if(n==0)
+        {
+            return false;
+        }
+        if(n%2==0)
+        {
+            return isUgly(n/2);
+        }
+        if(n%3==0)
+        {
+            return isUgly(n/3);
+        }
+        if(n%5==0)
+        {
+            return isUgly(n/5);
+        }
+        return false;
+    }
+}
